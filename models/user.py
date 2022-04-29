c28e0ccf0638657702f482f3a47b76586e3778fe
iNeedNew <official.andrei.nagornyi@gmail.com>
2022-04-29 21:42:54 +0300

    add methods user

--- models/user.py
+++ models/user.py
@@ -2,3 +2,9 @@ class User:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+
+    def get_user():
+        return 'user1'
+
+    def get_users():
+        return ['user1', 'user2', 'user3']
