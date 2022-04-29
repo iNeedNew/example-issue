a322151b684f4cfc4117c98bc94878cea8b4f682
iNeedNew <official.andrei.nagornyi@gmail.com>
2022-04-29 21:16:40 +0300

    create models post

---
+++ models/post.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self, post_id, title, content, user_id):
+        self.id = post_id
+        self.title = title
+        self.content = content
+        self.user_id = user_id
